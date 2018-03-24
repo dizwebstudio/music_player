0a1f96a7593db3f89ab3ea22a9216f8bae07f6b2
dizwebstudio <info@dizwebstudio.ru>
2018-03-25 02:01:10 +0300

    first interface

--- widget.cpp
+++ widget.cpp
@@ -6,9 +6,42 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    //******инициализируем tableView***//
+    m_playlistview = new QTableView;
+    //******инициализируем кнопки******//
+    m_pushbutton_add = new QPushButton("add");
+    m_pushbutton_play = new QPushButton("play");
+    m_pushbutton_stop = new QPushButton("stop");
+    m_pushbutton_next = new QPushButton("next");
+    m_pushbutton_prvw = new QPushButton("prev");
+
+    //*******Инициализируем layout*****//
+    m_hlayout = new QHBoxLayout;
+    m_vlayout = new QVBoxLayout;
+
+    //*******Добавим кнопки в layout***//
+    m_hlayout->addWidget(m_pushbutton_add);
+    m_hlayout->addWidget(m_pushbutton_play);
+    m_hlayout->addWidget(m_pushbutton_stop);
+    m_hlayout->addWidget(m_pushbutton_next);
+    m_hlayout->addWidget(m_pushbutton_prvw);
+    m_vlayout->addLayout(m_hlayout);
+    m_vlayout->addWidget(m_playlistview);
+
+    //добавим layout на widget
+    this->setLayout(m_vlayout);
+
+    //настройка таблицы плейлиста
+    m_playlistmodel = new QStandardItemModel(this);
+
+    m_playlistmodel->setHorizontalHeaderLabels(QStringList()<<tr("Audio Track")<<tr("File Path"));
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::on_btn_add_clicked(){
+
 }
--- widget.h
+++ widget.h
@@ -2,6 +2,15 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QStandardItemModel>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QFileDialog>
+#include <QDir>
+#include <QTableView>
 
 namespace Ui {
 class Widget;
@@ -17,6 +26,24 @@ public:
 
 private:
     Ui::Widget *ui;
+    QStandardItemModel *m_playlistmodel;
+    QMediaPlayer *m_player;
+    QMediaPlaylist *m_playlist;
+
+
+    QTableView *m_playlistview;
+    //***********************************************************************************
+    QPushButton *m_pushbutton_play;
+    QPushButton *m_pushbutton_stop;
+    QPushButton *m_pushbutton_next;
+    QPushButton *m_pushbutton_prvw;
+
+    QPushButton *m_pushbutton_add;
+
+    QHBoxLayout *m_hlayout;
+    QVBoxLayout *m_vlayout;
+private slots:
+    void on_btn_add_clicked();
 };
 
 #endif // WIDGET_H
